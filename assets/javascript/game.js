454c4b5c90bef9bec6574ba775433d470409bafb
Zaid Alhir <zaidalhir@yahoo.com>
2019-10-16 23:43:10 -0500

    adding pic's in html, working on js

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+
+const targetScore = 120;
+
+$("#number-to-guss").text(targetScore);
+
+let counter = 0;
+
+const numberOption = [6, ,2 , 5, 10];
+
+for (let i = 0; i < numberOptions.length; i++) {
+
+
+}
+
+
+$(".pok1").on("click", function() {
+
+    counter += increment;
+
+    alert("New score; " + counter);
+    
+
+    if (counter === targetNumber) {
+        alert("You win!");
+      }
+  
+      else if (counter >= targetNumber) {
+        alert("You lose!!");
+      }
+  
+});
+
+
+
+
+
